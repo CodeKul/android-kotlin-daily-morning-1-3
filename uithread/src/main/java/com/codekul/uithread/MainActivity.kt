3ea9869aa2f32ee3943f6c4f884042741cd8d6ee
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-08 15:39:42 +0530

    Ui thread and notifications

--- uithread/src/main/java/com/codekul/uithread/MainActivity.kt
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.kt
@@ -1,15 +1,62 @@
 package com.codekul.uithread
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.stream.IntStream
 
 class MainActivity : AppCompatActivity() {
+
+
+    var hand: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnOkay
+        hand = Handler(Looper.getMainLooper())
+    }
+
+    fun onOkay(vw: View) {
+        MyTask().execute(0, 10/*params*/)
+    }
+
+    fun handlerWay() {
+        Thread {
+            for (i in 1..10) {
+                Thread.sleep(2500)
+                hand?.post { txtNm.text = """ $i """ }
+            }
+        }.start()
+    }
+
+    class MyTask : AsyncTask<Int/*params*/, Int/*progress*/, Boolean/*Result*/>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            // UI Thread
+        }
+
+        override fun doInBackground(vararg progress: Int?/*params*/): Boolean/*Result*/ {
+            //Worker Thread
+
+            return true
+        }
+
+        override fun onPostExecute(result: Boolean?/*result*/) {
+            super.onPostExecute(result)
+
+            //UI Thread
+        }
+
+        override fun onProgressUpdate(vararg values: Int?/*progress*/) {
+            super.onProgressUpdate(*values)
+
+            //UI Thread
+        }
     }
 }
