d740a3f13458ca50d4dcb6c49221764f20d0bf65
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-21 14:14:10 +0530

    runtime permissions

--- contentprovider/src/main/java/com/codekul/contentprovider/MainActivity.kt
+++ contentprovider/src/main/java/com/codekul/contentprovider/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.codekul.contentprovider
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.ArrayAdapter
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.listView
 import org.jetbrains.anko.verticalLayout
 
@@ -12,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        readCustom()
+        readContact()
     }
 
     private fun readCustom() {
@@ -54,7 +59,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun readContact() {
+    private fun readContact() {
+
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                alert {
+                    title = "Need Your permission"
+                    message = "Wanna read contacts, Dont worry wont steal ur data"
+                    iconResource = R.mipmap.ic_launcher
+                    positiveButton("yes") {
+
+                        ActivityCompat.requestPermissions(this@MainActivity,
+                                arrayOf(Manifest.permission.READ_CONTACTS),
+                                1234)
+                    }
+                }.show()
+
+            } else {
+
+                ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(Manifest.permission.READ_CONTACTS),
+                        1234)
+            }
+        } else redCon()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1234) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                redCon()
+            }
+        }
+    }
+
+    private fun redCon() {
         val dtSt = ArrayList<String>()
 
         val crsr = contentResolver.query(
