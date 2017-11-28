c4168efbc66b7e7e52b4d5fdfe547846a18fc918
aniruddha <aniruddha.kudalkar@gmail.com>
2017-11-28 15:02:54 +0530

    http post using volley

--- http/src/main/java/com/codekul/http/MainActivity.kt
+++ http/src/main/java/com/codekul/http/MainActivity.kt
@@ -5,11 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.codekul.http.dto.Sample
 import com.google.gson.Gson
+import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.indeterminateProgressDialog
+import org.jetbrains.anko.progressDialog
+import org.jetbrains.anko.toast
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,9 +28,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         q = Volley.newRequestQueue(this)
+        gs = Gson()
     }
 
     fun onGet(view: View?) {
+
+        val pd = indeterminateProgressDialog(
+                message = "Weather",
+                title = """ fetching temperature for ${edNm.text}"""
+        )
 
         val url = """ http://api.openweathermap.org/data/2.5/weather?q=${edNm.text}&APPID=7406c21bb1cb9f59d936a59c4e890279 """
         q?.add(
@@ -31,7 +44,28 @@ class MainActivity : AppCompatActivity() {
                     Log.i("@codekul", """ $it """)
                     val sm = gs?.fromJson<Sample>(it, Sample::class.java)
                     txtInf.text = """ ${sm?.main?.temp} """
-                }, {})
+
+                    postData(it)
+
+                    pd.dismiss()
+                }, {
+                    pd.dismiss()
+                })
+        )
+    }
+
+    private fun postData(dt : String) {
+        val obj = JSONObject()
+        obj.put("json",dt)
+
+        val url = "https://digital-shelter-153912.firebaseio.com/helloWeather.json"
+        q?.add(
+                JsonObjectRequest(
+                        url, obj, {
+                    toast("Post Success")
+                },{
+                    toast("Post Error")
+                })
         )
     }
 }
